563e7b8ffd5a8550f4136f11fea2b1310f44862b
Jurriaan Bremer <jbr@cuckoo.sh>
2017-04-20 15:50:50 +0200

    202a2

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ def do_setup(**kwargs):
 
 do_setup(
     name="Cuckoo",
-    version="2.0.2a1",
+    version="2.0.2a2",
     author="Stichting Cuckoo Foundation",
     author_email="cuckoo@cuckoofoundation.org",
     packages=[
